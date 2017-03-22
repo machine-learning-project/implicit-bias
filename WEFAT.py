7e2fcaf59a7948f9c6131d531558b8a0f165225f
Mi Fang <fangm1994@gmail.com>
2017-03-21 21:11:43 -0400

    1. WEFAT.py — re-arrange the code structure to export functions in module. 2. WEFAT_Judge.py — add using word vectors generated from major opinion text data to implement WEFAT.

--- WEFAT.py
+++ WEFAT.py
@@ -1,18 +1,5 @@
 import math
 
-# trained word vectors, common crawl
-glove = open('glove.840B.300d.txt')
-w_f = open('target_words')
-a_f = open('attribute_a')
-b_f = open('attribute_b')
-
-# target words
-W = w_f.readlines()[0].strip().split(', ')
-
-# attribute words
-A = a_f.readlines()[0].strip().split(', ')
-B = b_f.readlines()[0].strip().split(', ')
-
 # func for calcuate cos distance of two vectors
 def cos(x,y):
     s = 0
@@ -38,46 +25,64 @@ def std_dev(w,X):
     cos_avg = sum(cos_rst)/len(cos_rst)
     return sum((cos_avg-x)**2 for x in cos_rst)/len(cos_rst)
 
-# goal: calculate s(w, A, B) using formula at pg.10 Semantics derived automatically from language corpora necessarily contain human biases
+def load_from_glove(W, A, B):
+    g_a = []
+    g_b = []
+    g_w = []
+    g_x = []    
+    
+    print "loading file"
 
-# 1. traverse the glove file and get the cooresponding vectors
-g_a=[]
-g_b=[]
-g_x=[]
-g_w=[]
+    for line in open('glove.840B.300d.txt'):
+        v = line.strip().split(' ')
+        wi = v[0]
+        v = [float(a) for a in v[1:]]
+        for w in W:
+            if wi == w:
+                g_w.append(v)
+        for w in A:
+            if wi == w:
+                g_a.append(v)
+        for w in B:
+            if wi == w:
+                g_b.append(v)
 
-print "loading file"
-lines = glove.readlines()
+    g_x = g_a + g_b
 
-for l in lines:
-    v = l.strip().split(' ')
-    wi = v[0]
-    v = [float(a) for a in v[1:]]
-    for w in W:
-        if wi == w:
-            g_w.append(v)
-    for w in A:
-        if wi == w:
-            g_a.append(v)
-    for w in B:
-        if wi == w:
-            g_b.append(v)
+    print "finished loading"
+    return g_a, g_b, g_x, g_w
 
-g_x = g_a + g_b
+def wefat(load_word_vector):
+    # trained word vectors, common crawl
+    w_f = open('target_words')
+    a_f = open('attribute_a')
+    b_f = open('attribute_b')
 
-print "finished loading"
-#print g_a
-#print g_b
-#print g_x
+    # target words
+    W = w_f.readlines()[0].strip().split(', ')
 
-# 2. get mean(cos(w,a)), mean(cos(w,b)
-# 3. calculate std-dev(cos(w,x))
-s_v = []
-# s values:
-for w in g_w:
-    s_v.append((mean_cos(w,g_a)-mean_cos(w,g_b))/std_dev(w,g_x))
-print s_v
+    # attribute words
+    A = a_f.readlines()[0].strip().split(', ')
+    B = b_f.readlines()[0].strip().split(', ')
 
-w_f.close()
-a_f.close()
-b_f.close()
+    # goal: calculate s(w, A, B) using formula at pg.10 Semantics derived automatically from language corpora necessarily contain human biases
+
+    # 1. traverse the glove file and get the cooresponding vectors
+    g_a, g_b, g_x, g_w = load_word_vector(W, A, B)
+    # 2. get mean(cos(w,a)), mean(cos(w,b)
+    # 3. calculate std-dev(cos(w,x))
+    s_v = []
+    # s values:
+    for w in g_w:
+        s_v.append((mean_cos(w,g_a)-mean_cos(w,g_b))/std_dev(w,g_x))
+    print s_v
+
+    w_f.close()
+    a_f.close()
+    b_f.close()
+
+def main():
+    wefat(load_from_glove)
+
+if __name__ == "__main__":
+    main()
--- WEFAT_Judge.py
+++ WEFAT_Judge.py
@@ -4,28 +4,33 @@ from glob import glob
 from zipfile import ZipFile
 import sys  
 import re
+import WEFAT
 
 # set default utf8 encoding to open file
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
+# sentence generator from text
 class Sentence(object):
 	def __init__(self, dirname):
 		self.dirname = dirname
+		# iterate through all the zip files (different years)
+		self.zipfiles = glob(dirname + '/*zip')
 
 	# normalization
 	def normalize(self, text):
 		# split on non-alphanumeric characters(space and punctuation)
 		norm_words = re.split('\W+', text.lower())
+
+		# can also remove stopwords
+		
 		return norm_words
 
 	# get word from text
 	def __iter__(self):
-		# iterate through all the zip files (different years)
-		os.chdir(self.dirname)
-		zipfiles = glob('*zip')
 
-		for zfname in zipfiles:
+		for zfname in self.zipfiles:
+
 			zfile = ZipFile(zfname)
 			members = zfile.namelist()
 			# iterate through all texts in zip file
@@ -33,43 +38,65 @@ class Sentence(object):
 				# use majority opinions text
 				if not fname.endswith('-maj.txt'):
 					continue
-	            # open file and read line by line
+				print fname	            
+				# open file and read line by line
 				with zfile.open(fname) as f:
-					for line in f:      
+					for line in f:    
 						yield self.normalize(line)
 
-def train_model(text_dir):
-	# generate words from text
-	sentences = Sentence(text_dir)
+class WVModel(object):
 
-	# train word vectors
-	model = gensim.models.Word2Vec(sentences)
-	model.save(model_name)
+	def __init__(self, text_dir):
+		self.text_dir = text_dir
+		self.model_name = 'tmp/mymodel'
 
-def use_model():
-	# load model
-	model = gensim.models.Word2Vec.load(model_name)
+	def train_model(self):
+		# generate words from text
+		sentences = Sentence(self.text_dir)
+		# train word vectors
+		print 'training model...'
+		model = gensim.models.Word2Vec(sentences)
+		# save model
+		print 'saving model...'
+		model.save(self.model_name)
 
-	w_f = open('target_words')
-	a_f = open('attribute_a')
-	b_f = open('attribute_b')
+	def load_word_vector(self, W, A, B):
+		g_a = []
+		g_b = []
+		g_w = []
+		g_x = []
 
-	# target words
-	W = w_f.readlines()[0].strip().split(', ')
+		# get word vectors from model
+		for w in W:
+			if w in self.model.wv.vocab:
+				g_w.append(self.model[w])
+		for a in A:
+			if a in self.model.wv.vocab:
+				g_a.append(self.model[a])
+		for b in B:
+			if b in self.model.wv.vocab:
+				g_b.append(self.model[b])			
 
-	# attribute words
-	A = a_f.readlines()[0].strip().split(', ')
-	B = b_f.readlines()[0].strip().split(', ')
+		g_x = g_a + g_b
 
-	# unfinished
-	# use similarity() to measure the similarity between word vectors
-	# model.similarity()
+		return g_a, g_b, g_x, g_w
 
+	def use_model(self):
+		# load model
+		self.model = gensim.models.Word2Vec.load(self.model_name)
 
-text_dir = 'cleaned'
-model_name = 'mymodel'
-train_model(text_dir)
+		# use WEFAT method
+		WEFAT.wefat(self.load_word_vector)
 
+def main():
+	# directory for text data 
+	text_dir = 'cleaned'
 
+	wvmodel = WVModel(text_dir)
+	wvmodel.train_model()
+	# wvmodel.use_model()
+
+if __name__ == "__main__":
+	main()
 
 
